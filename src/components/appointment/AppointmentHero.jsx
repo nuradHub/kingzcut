4ce6a12ab9b79e68e76b692ccc1233b4baba5240
nuradHub <saliunurudeen25@gmail.com>
2025-11-11 16:33:15 +0100

    Import About.css in AppointmentHero component

--- src/components/appointment/AppointmentHero.jsx
+++ src/components/appointment/AppointmentHero.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Kinzcut from "../../assets/img/kingz-cut-main.png";
 import Appointment from "./Appointment";
 import './AppointmentHero.css'
+import '../About.css'
 
 const AppointmentHero = ()=> {
 
